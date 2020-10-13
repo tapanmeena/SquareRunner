bc59899a9d96f2818a2f8bd711916bcf3f379a65
Tapan Meena <tapanmeena1998@gmail.com>
2020-10-13 22:43:24 +0530

    Added Object destroyer in Trial screen

---
+++ Assets/CameraMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey("w"))
+            rb.AddForce(0, 0, 500f * Time.fixedDeltaTime);
+        if (Input.GetKey("s"))
+            rb.AddForce(0, 0, -500f * Time.fixedDeltaTime);
+
+    }
+}
